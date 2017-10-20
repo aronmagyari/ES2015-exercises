2745b0b3f32b95323e1dd5d90d776ceaf07bb8bf
Ferencz Magyari <afmagyari@gmail.com>
2017-10-19 23:39:58 -0700

    start async functions exercise

---
+++ async-functions.js
@@ -0,0 +1,42 @@
+// 
+// hasMostFollowers('elie','tigarcia','colt').then(function(data){
+//     console.log(data)
+// });
+// 
+// "Colt has the most followers with 424"
+// 
+
+
+
+// 
+// starWarsString(1).then(function(data){
+//     console.log(data)
+// })
+// 
+// "Luke Skywalker"
+// 
+
+
+
+// 
+// BONUS 1
+// 
+// starWarsString(1).then(function(data){
+//     console.log(data)
+// })
+// 
+// "Luke Skywalker is featured in The Empire Strikes Back, directed by Irvin Kershner"
+// 
+
+
+
+// 
+// BONUS 2
+// 
+// starWarsString(1).then(function(data){
+//     console.log(data)
+// })
+// 
+// "Luke Skywalker is featured in The Empire Strikes Back, directed by Irvin Kershner and it takes place on Hoth"
+// 
+
