f7c24270d13770dfb609a383dcebb0131910e5b4
Ferencz Magyari <afmagyari@gmail.com>
2017-10-19 23:58:45 -0700

    refractor promises to be use async/await

--- async-functions.js
+++ async-functions.js
@@ -7,27 +7,13 @@
 // 
 
 
-
-// 
-// starWarsString(1).then(function(data){
-//     console.log(data)
-// })
-// 
-// "Luke Skywalker"
-// 
-
-
-
-// 
-// BONUS 1
-// 
-// starWarsString(1).then(function(data){
-//     console.log(data)
-// })
-// 
-// "Luke Skywalker is featured in The Empire Strikes Back, directed by Irvin Kershner"
-// 
-
+async function getMostFollowers(...users) {
+	let url = "https://api.github.com/users/";
+	let profilePromises = users.map(username => $.getJSON(url + username));
+	let profiles = await Promise.all(profilePromises);
+	let user = data.sort((a,b) => a.followers < b.followers)[0];
+	return `${user.ame} has the most followers with ${user.followers}`
+}
 
 
 // 
@@ -40,3 +26,13 @@
 // "Luke Skywalker is featured in The Empire Strikes Back, directed by Irvin Kershner and it takes place on Hoth"
 // 
 
+async function starWarsString(id) {
+	let str = '';
+	let person = await $.getJSON(`https://swapi.co/api/people/${id}`);
+	str += `${person.name} is featured in `;
+	let film = await $.getJSON(person.films[0]);
+	str += `${film.title}, directed by ${film.director} `;
+	let planet = $.getJSON(film.planets[0]);
+	str += `and it takes place on ${planet.name}`;
+	return str;
+}
